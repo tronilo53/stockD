5ee4c49bd2605c0cf436d99ce43f20881532819d
Tronilo <Luis.Tronilo@henryschein.es>
2024-04-17 18:14:04 +0200

    Instalado modulo de chart.js. Diseñando página de Home

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public chart: any = [];
+  private meses: string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+  private datosPedidos: number[] = [2, 3, 7, 1, 12, 7, 5, 2, 11, 3, 2, 4];
+  ngOnInit(): void {
+    this.chartPedidos();
+  }
+  private chartPedidos(): void {
+    this.chart = new Chart('pedidos', {
+      type: 'bar',
+      data: {
+        labels: this.meses,
+        datasets: [
+          {
+            label: 'Recuento',
+            data: this. datosPedidos,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 }
