71cea78dd34e2fbca7bc26afc052d7abfd713fe7
Tronilo <Luis.Tronilo@henryschein.es>
2024-05-20 17:38:51 +0200

    En curso con pagina de stock de componentes

--- src/app/pages/dashboard/componentes/componentes.component.ts
+++ src/app/pages/dashboard/componentes/componentes.component.ts
@@ -9,6 +9,7 @@ import { environment } from '../../../../environments/environment';
 export class ComponentesComponent implements OnInit {
 
   public categorias: string[] = environment.categorias;
+  public empresas: string[] = environment.empresas;
 
   constructor() {}
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     url: 'https://freelsdevcamp.es/',
     meses: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
     anios: ['2023', '2024'],
-    categorias: ['Altavoces', 'Cables', 'Carcasas', 'Circuitos Integrados', 'Compuestos Encapsulantes']
+    categorias: ['Altavoces', 'Cables', 'Carcasas', 'Circuitos Integrados', 'Compuestos Encapsulantes'],
+    empresas: ['Amazon', 'Farnell', 'Rs']
 };
